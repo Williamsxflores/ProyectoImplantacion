d15d32e621b8f2d870c63c2ff31d2fdaad27fff3
EdwardCraft <eduardo.flores81@uabc.edu.mx>
2022-11-27 14:52:41 -0800

    code spliting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, Suspense } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -6,16 +6,27 @@ import {
   Switch,
 } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
+import LoadingSpinner from "shared/components/UIElements/LoadingSpinner";
 
-import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
-import UserPlaces from "./places/pages/UserPlaces";
-import UpdatePlace from "./places/pages/UpdatePlace";
-import SearchScreen from "./places/pages/SearchScreen";
-import RegisterBusiness from "places/pages/RegisterBusiness";
-import Auth from "./user/pages/Auth";
+// import Users from "./user/pages/Users";
+// import NewPlace from "./places/pages/NewPlace";
+// import UserPlaces from "./places/pages/UserPlaces";
+// import UpdatePlace from "./places/pages/UpdatePlace";
+// import SearchScreen from "./places/pages/SearchScreen";
+// import RegisterBusiness from "places/pages/RegisterBusiness";
+// import Auth from "./user/pages/Auth";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import { AuthContext } from "./shared/context/auth-context";
+
+const Users = React.lazy(() => import("./user/pages/Users"));
+const NewPlace = React.lazy(() => import("./places/pages/NewPlace"));
+const UserPlaces = React.lazy(() => import("./places/pages/UserPlaces"));
+const UpdatePlace = React.lazy(() => import("./places/pages/UpdatePlace"));
+const SearchScreen = React.lazy(() => import("./places/pages/SearchScreen"));
+const RegisterBusiness = React.lazy(() =>
+  import("places/pages/RegisterBusiness")
+);
+const Auth = React.lazy(() => import("./user/pages/Auth"));
 
 toast.configure();
 
@@ -90,7 +101,17 @@ const App = () => {
     >
       <Router>
         <MainNavigation />
-        <main>{routes}</main>
+        <main>
+          <Suspense
+            fallback={
+              <div className="center">
+                <LoadingSpinner />
+              </div>
+            }
+          >
+            {routes}
+          </Suspense>
+        </main>
       </Router>
       <ToastContainer />
     </AuthContext.Provider>
