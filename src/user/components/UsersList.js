d12f058a1ee7656d171f49367c164cdaaf1b9eef
EdwardCraft <eduardo.flores81@uabc.edu.mx>
2022-11-27 17:43:11 -0800

    change text in home page

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -65,38 +65,20 @@ const UsersList = (props) => {
             </p>
           </div>
           <div className="description">
-            <h2>Centro de atencion para la empresa</h2>
+            <h5>Centro de atencion para la empresa</h5>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+              Desde estas areas puede comunicarse con nosotros para cualquier
+              duda que pueda surgir Email:
             </p>
-          </div>
-          <div className="description">
-            <h2>Centro de atencion para la empresa</h2>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+              Telefono: <strong>661-125-7396</strong>
+            </p>
+            <p>
+              Email: <strong>swanalytics@gmail.com</strong>
             </p>
           </div>
         </div>
       </section>
-
       <section className="ContainerUserList">
         <ul className="Userslist">
           {users.map((user) => (
